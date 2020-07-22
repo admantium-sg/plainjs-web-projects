ae7a11558d7c93573a628dc723e4784e22fc40dc
devcon <devcon@admantium.com>
2020-07-22 19:56:54 +0200

    update: load state from local storage

--- 02_seat_booking/script.js
+++ 02_seat_booking/script.js
@@ -6,30 +6,60 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = parseInt(movieSelect.value);
 
+loadState();
+updateSelectedCount();
+
 console.log("Ticket Price", ticketPrice, typeof ticketPrice);
 
-function updateStorage(key, json) {
+// Storage abstractions
+function updateState(key, json) {
   localStorage.setItem(key, json);
 }
 
+function readState(key) {
+  return localStorage.getItem(key);
+}
+
+// Load state
+function loadState() {
+  const selectedSeats = JSON.parse(readState('selectedSeats'));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected')
+      }
+    })
+  }
+
+  const selectedMovieIndex = readState('selectedMovieIndex');
+
+  if (selectedMovieIndex !== null && typeof selectedMovieIndex === 'number') {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
+// Updates ticket price
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
   const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
 
-  updateStorage('selectedSeats', JSON.stringify(seatsIndex));
+  updateState('selectedSeats', JSON.stringify(seatsIndex));
 
   count.innerText = selectedSeats.length;
   total.innerText = selectedSeats.length * ticketPrice;
 }
 
+// Change Movie
 movieSelect.addEventListener('change', e => {
   ticketPrice = parseInt(e.target.value);
-  updateStorage('selectedMovieIndex', e.target.selectedIndex);
-  updateStorage('selectedMoviePrice', e.target.value);
+  updateState('selectedMovieIndex', e.target.selectedIndex);
+  updateState('selectedMoviePrice', e.target.value);
   updateSelectedCount();
 })
 
+// Click Seats
 container.addEventListener('click', (e) => {
   const classes = e.target.classList
   if (classes.contains('seat') && !classes.contains('occupied')) {
