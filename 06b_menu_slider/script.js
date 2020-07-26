f7588dcdac1448ed41689023a8bcda048f922d44
devcon <devcon@admantium.com>
2020-07-26 12:03:47 +0200

    update: display modal

--- 06b_menu_slider/script.js
+++ 06b_menu_slider/script.js
@@ -1,5 +1,16 @@
 const toggle = document.getElementById('toggle');
+const modalContainer = document.getElementById('modal-container');
+const openButton = document.getElementById('open-button');
+const closeButton = document.getElementById('close-button');
 
 toggle.addEventListener('click', () => {
   document.body.classList.toggle('show-nav');
+})
+
+openButton.addEventListener('click', () => {
+  modalContainer.classList.add('show-modal');
+})
+
+closeButton.addEventListener('click', () => {
+  modalContainer.classList.remove('show-modal');
 })
