74ea53acfa499eb77652045b60508f9e1e682db0
devcon <devcon@admantium.com>
2020-08-16 19:07:12 +0200

    update: fetch and render posts when scrolled down

--- 10_infinite_scrolling/script.js
+++ 10_infinite_scrolling/script.js
@@ -1,13 +1,15 @@
-const postContainer = document.getElementById('posts-container')
+const postContainer = document.getElementById('posts-container');
+const loader = document.getElementById('loader');
 
-var limit = 5
-var page = 1
+var limit = 5;
+var page = 1;
 
 async function loadPosts () {
+  console.log('load posts');
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page++}`
-  )
-  const posts = await res.json()
+  );
+  const posts = await res.json();
 
   posts.forEach(post => {
     const html = `
@@ -15,14 +17,44 @@ async function loadPosts () {
         <div class="post-info">
           <h2 class="post-title">${post.title}</h2>
           <p class="post-body">${post.body}</p>
-      </div>`
+      </div>`;
 
-    const node = document.createElement('div')
-    node.classList.add('post')
-    node.innerHTML = html
+    const node = document.createElement('div');
+    node.classList.add('post');
+    node.innerHTML = html;
 
-    postContainer.appendChild(node)
-  })
+    postContainer.appendChild(node);
+  });
 }
 
-loadPosts()
+function isVisible (domElement) {
+  var bounding = domElement.getBoundingClientRect();
+  return (
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    bounding.right <=
+      (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function showLoader () {
+  if (!loader.classList.contains('show')) {
+    loader.classList.add('show');
+
+    setTimeout(() => {
+      loader.classList.remove('show');
+      loadPosts();
+    }, 1500);
+  }
+}
+
+window.addEventListener('scroll', () => {
+  if (isVisible(loader)) {
+    console.log('Loader visible?', isVisible(loader));
+    showLoader();
+  }
+});
+
+loadPosts();
